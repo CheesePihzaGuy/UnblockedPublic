72f3c0b81353b36ca65a4087f578e0e5d4d14747
CheesePihzaGuy <149946709+CheesePihzaGuy@users.noreply.github.com>
2024-09-28 21:45:07 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,1 +1,10 @@
-
+<!DOCTYPE php>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Unblocked V2</title>
+  <h1>Signup or Login is unavailable</h1>
+  <h4>Will be available soon!</h3>
+  <h7>Coming with update 2024.9f -- Part 4</h6>
+</head>
